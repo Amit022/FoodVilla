aca656c6ffa43debaaab1768030317a619ea72ca
Amit Chowdhury <62304454+Amit022@users.noreply.github.com>
2023-07-07 13:10:24 +0530

    Revert "Food Ordering App"
    This reverts commit 9bc6475ebc9e7f2a90f855b1385429ebccba408e.

--- src - Copy/components/__tests__/sum.test.js
+++
@@ -1,9 +0,0 @@
-import { sum } from "../sum"
-
-test("Sum of two nos", ()=> {
-    expect(sum(2,3)).toBe(5);
-})
-
-// test("sum of two nos", ()=> {
-//     expect(sum(2,3)).toBe(5)
-// })
--- src - Copy/utils/useContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const userInfo = createContext({
-    user:{
-        name: "dummy name",
-        email:"dummy@gmail.com"
-    }
-})
-export default userInfo;
